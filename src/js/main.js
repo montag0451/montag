296f38f67c94da102b5328b31de2bfa02cdcc57e
montag0451 <andrey11man@gmail.com>
2020-08-26 07:12:49 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-//here will by JavaScript code ...
+$(document).ready(function(){
+    $('.slick-vertical').slick({
+        vertical: true,
+        slidesToShow: 2,
+        autoplay: true,
+        prevArrow: '<img src="/img/up.svg">',
+        nextArrow: '<img src="/img/down.svg">'
+    });
+  });
+          
